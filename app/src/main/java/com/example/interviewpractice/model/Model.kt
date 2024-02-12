11a99e8cc6e74b4a234e63eeb86bdd2787f01178
Derek <dmaxin@shaw.ca>
2024-02-11 20:59:46 -0500

    login screen

---
+++ app/src/main/java/com/example/interviewpractice/model/Model.kt
@@ -0,0 +1,4 @@
+package com.example.interviewpractice.model
+
+class Model {
+}
--- app/src/main/java/com/example/interviewpractice/ui/elements/question.kt
+++ app/src/main/java/com/example/interviewpractice/ui/elements/question.kt
@@ -1,4 +1,4 @@
-package com.example.interviewpractice.elements
+package com.example.interviewpractice.ui.elements
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
