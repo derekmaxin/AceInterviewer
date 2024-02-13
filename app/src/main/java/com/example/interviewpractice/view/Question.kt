aef6b60e2dc5f3035907d507040d6eb1c2dcaea6
mapuntil <mapuntil@uwaterloo.ca>
2024-02-13 17:22:55 -0500

    Unfinished changes to Question and QVM

--- app/src/main/java/com/example/interviewpractice/view/Question.kt
+++ app/src/main/java/com/example/interviewpractice/view/Question.kt
@@ -5,21 +5,33 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.interviewpractice.viewmodel.QuestionViewModel
 
 @Composable
-@Preview
-fun Question() {
+// @Preview
+fun Question(questionViewModel: QuestionViewModel) {
+    val viewModel by remember { mutableStateOf(questionViewModel) }
+
     Card() {
         Text(
-            text = "Question that might be too long to fill the screen",
+            text = viewModel.questionText,
             modifier = Modifier
                 .padding(12.dp),
-            textAlign = TextAlign.Center,
+            textAlign = TextAlign.Left,
             style = MaterialTheme.typography.bodySmall,
         )
+        if (showTags) {
+            Card() {
+
+            }
+        }
+
     }
 }
