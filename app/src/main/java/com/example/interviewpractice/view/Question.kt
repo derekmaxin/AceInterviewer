2e9e3117909cd9bc70cf027b233f9a60cd9cf9f9
mapuntil <mapuntil@uwaterloo.ca>
2024-02-13 18:10:09 -0500

    Made functioning modular question component

--- app/src/main/java/com/example/interviewpractice/view/Question.kt
+++ app/src/main/java/com/example/interviewpractice/view/Question.kt
@@ -1,7 +1,15 @@
 package com.example.interviewpractice.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -15,23 +23,55 @@ import androidx.compose.ui.unit.dp
 import com.example.interviewpractice.viewmodel.QuestionViewModel
 
 @Composable
-// @Preview
+fun Tag(text: String) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        )
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier
+                .padding(12.dp, 6.dp, 12.dp, 6.dp),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodySmall,
+        )
+    }
+}
+
+@Composable
 fun Question(questionViewModel: QuestionViewModel) {
     val viewModel by remember { mutableStateOf(questionViewModel) }
 
-    Card() {
-        Text(
-            text = viewModel.questionText,
+    Card(
+        modifier = Modifier
+            .padding(12.dp)
+    ) {
+        Column(
             modifier = Modifier
-                .padding(12.dp),
-            textAlign = TextAlign.Left,
-            style = MaterialTheme.typography.bodySmall,
-        )
-        if (showTags) {
-            Card() {
-
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(6.dp)
+        ) {
+            Text(
+                text =  viewModel.questionText.value,
+                modifier = Modifier
+                    .padding(4.dp,0.dp,4.dp,0.dp),
+                textAlign = TextAlign.Left,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+            if (viewModel.showTags.value) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(6.dp)
+                ) {
+                    for (tag in viewModel.tags.value) {
+                        Tag(tag)
+                    }
+                }
             }
         }
+
 
     }
 }
