966bb139432781d538731a919228b72c5e4c46ac
Derek <dmaxin@shaw.ca>
2024-02-15 16:46:48 -0500

    registration screen

--- app/src/main/java/com/example/interviewpractice/view/Login.kt
+++ app/src/main/java/com/example/interviewpractice/view/Login.kt
@@ -2,12 +2,9 @@ package com.example.interviewpractice.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.graphics.Color
@@ -20,9 +17,7 @@ import com.example.interviewpractice.R
 @Composable
 @Preview
 fun LoginScreen() {
-    Surface(
-
-    ) {
+    Surface {
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -32,41 +27,43 @@ fun LoginScreen() {
             Image(
                 painter = painterResource(id = R.drawable.klee),
                 contentDescription = "Your Image",
-                modifier = Modifier.fillMaxWidth().height(500.dp).padding(vertical = 8.dp),
+                modifier = Modifier.fillMaxWidth().height(450.dp).padding(vertical = 8.dp),
             )
             TextField(
-                value = "username",
+                value = "",
                 onValueChange = { },
-                label = { Text("username") },
+                label = { Text("Username") },
                 modifier = Modifier.fillMaxWidth().height(85.dp).padding(vertical = 8.dp),
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Next,
-                ),
-                keyboardActions = KeyboardActions(
-                    onNext = { }
-                ),
                 textStyle = TextStyle(fontSize = 28.sp)
             )
             TextField(
-                value = "password",
+                value = "",
                 onValueChange = { },
                 label = { Text("Password") },
                 modifier = Modifier.fillMaxWidth().height(85.dp).padding(vertical = 8.dp),
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Done,
-                ),
-                keyboardActions = KeyboardActions(
-                    onDone = { }
-                ),
                 textStyle = TextStyle(fontSize = 28.sp)
             )
             Button(
                 onClick = { },
-                modifier = Modifier.fillMaxWidth().height(100.dp).padding(vertical = 8.dp)
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent)
+            ) {
+                Text("Forgot Password?",
+                    style = TextStyle(
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Light,
+                        color = Color.Magenta,
+                    ),
+                    modifier = Modifier.fillMaxWidth().height(20.dp).padding(vertical = 0.dp),
+                )
+            }
+            Button(
+                onClick = { },
+                modifier = Modifier.fillMaxWidth().height(80.dp).padding(vertical = 8.dp)
             ) {
                 Text("Log in",
                     style = TextStyle(
-                        fontSize = 32.sp,
+                        fontSize = 28.sp,
                         color = Color.White,
                     )
                 )
