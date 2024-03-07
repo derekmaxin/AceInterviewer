75bab5115bba3899534a4f8ac0cfa97a5d507098
mapuntil <mapuntil@uwaterloo.ca>
2024-03-07 12:14:19 -0500

    Added notifications screen

--- app/src/main/java/com/example/interviewpractice/frontend/components/notification/NotificationGroupViewModel.kt
+++ app/src/main/java/com/example/interviewpractice/frontend/components/notification/NotificationGroupViewModel.kt
@@ -2,11 +2,10 @@ package com.example.interviewpractice.frontend.components.review
 
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
-import com.example.interviewpractice.model.AuthModel
 import com.example.interviewpractice.frontend.Subscriber
 import com.example.interviewpractice.model.MainModel
 
-class NotificationViewModel(private val model: MainModel): Subscriber {
+class NotificationGroupViewModel(private val model: MainModel): Subscriber {
 
     var expanded = mutableStateOf(false)
     var notifications = mutableStateOf(listOf<String>(""))
