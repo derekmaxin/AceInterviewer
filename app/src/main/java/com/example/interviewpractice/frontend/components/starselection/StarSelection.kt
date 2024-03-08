6bcd67585a4ccfffa4fdd22466961e5ff8b64b3e
mapuntil <mapuntil@uwaterloo.ca>
2024-03-07 23:20:16 -0500

    Star selection version 2 (incomplete)

--- app/src/main/java/com/example/interviewpractice/frontend/components/starselection/StarSelection.kt
+++ app/src/main/java/com/example/interviewpractice/frontend/components/starselection/StarSelection.kt
@@ -3,8 +3,8 @@ package com.example.interviewpractice.frontend.components.starselection
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.StarOutline
 import androidx.compose.material3.Icon
@@ -13,18 +13,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Composable
 @Preview
-fun starSelection(text: String = "example") {
-    var score by remember { mutableIntStateOf(1) }
+fun StarSelection(text: String = "example") {
+    var score by remember { mutableStateOf( mutableListOf<Boolean>(false, false, false, false, false) ) }
 
     Row(
         modifier = Modifier.fillMaxWidth(),
@@ -44,22 +44,17 @@ fun starSelection(text: String = "example") {
         Row(
             horizontalArrangement = Arrangement.End,
         ) {
-            val unfilled_stars = 5 - score
+            for ((index, star) in score.withIndex()) {
+                IconButton(
+                    onClick = {
 
-            for ( i in 1..score ) {
-                IconButton(onClick = { score = i }) {
-                    Icon(Icons.Filled.Star,
-                        contentDescription = "Outlined Star",
-                    )
-                }
-
-            }
-
-            for ( i in score + 1..5) {
-                IconButton(onClick = { score = i }) {
-                    Icon(Icons.Filled.Star,
-                        contentDescription = "Outlined Star",
-                    )
+                        for (i in 0..index) {
+                            score[i] = true
+                        }
+                    }
+                ) {
+                    if (star) { Icon(Icons.Filled.Star, contentDescription = "Star") }
+                    else { Icon(Icons.Outlined.StarOutline, contentDescription = "Outlined Star") }
                 }
             }
         }
